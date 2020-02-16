00beeeb7afe20ee79439ec8b2998011a28904bd5
Kevin Skinner <nivek.skinner@gmail.com>
2020-02-15 17:38:17 -0800

    prep work for deployment

--- server.js
+++ server.js
@@ -7,17 +7,27 @@ const MOVIEDEX = require('./moviedex.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
-app.use(function validateBearerToken(req, res, next) {
+app.use(function validateBearerToken(error, req, res, next) {
     const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization') 
+    const authToken = req.get('Authorization')
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request' })
     }
+
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' }} 
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+
     next()
 })
 
@@ -45,7 +55,7 @@ app.get('/movie', function handleGetMovie(req, res) {
     res.json(response)
 })
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
